0190d134ae5e5b6f9dc2d5ac8b8799e839d1b074
flakmonkey <01.flakmonkey.10@gmail.com>
2013-05-18 20:51:12 -0700

    Added poll d detail view

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,19 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):
